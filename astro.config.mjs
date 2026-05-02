322c81d3bdbb2a1864e2f7240da999a8d1c0e0c2
Julian Atkin <jatkindev@gmail.com>
2026-05-02 11:49:42 -0400

    Test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,5 +16,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  adapter: cloudflare(),
+  // adapter: cloudflare(),
 });
